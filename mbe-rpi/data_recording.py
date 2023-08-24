f2d79a35540071343d5d1b598d4ea97432dbef59
SpacisBexus <spacis.bexus@gmail.com>
2023-08-24 20:06:45 +0100

    Add recording pps micros and datetime

--- mbe-rpi/data_recording.py
+++ mbe-rpi/data_recording.py
@@ -5,6 +5,7 @@ BASE_DIR = './records/'
 SENSOR_RECORD_SUFFIX = 'sensor_records'
 TEMPERATURE_RECORD_SUFFIX = 'temperature_records'
 GPS_RECORD_SUFFIX = 'gps_records'
+PPS_RECORD_SUFFIX = 'pps_records'
 
 class DataRecorder:
     def __init__(self):
@@ -14,23 +15,32 @@ class DataRecorder:
         self.sensor_record_path = BASE_DIR + '/' + SENSOR_RECORD_SUFFIX + '/' + SENSOR_RECORD_SUFFIX + self.time_stamp + '.csv'
         self.temperature_record_path = BASE_DIR + '/' + TEMPERATURE_RECORD_SUFFIX + '/' + TEMPERATURE_RECORD_SUFFIX + self.time_stamp + '.csv'
         self.gps_record_path = BASE_DIR + '/' + GPS_RECORD_SUFFIX + '/' + GPS_RECORD_SUFFIX + self.time_stamp + '.csv'
+        self.pps_record_path = BASE_DIR + '/' + PPS_RECORD_SUFFIX + '/' + PPS_RECORD_SUFFIX + self.time_stamp + '.csv'
 
         self.setup_sensor_record()
         self.setup_temperature_record()
-        self.setup_gps_record()  
+        self.setup_gps_record()
+        self.setup_pps_record()
 
     def setup_sensor_record(self):
         self.sensor_record_file = open(self.sensor_record_path, 'a')
         self.sensor_record_file.write('sensor_1,sensor_2,sensor_3,sensor_4,delay\n')
+        self.sensor_record_file.flush()
 
     def setup_temperature_record(self):
         self.temperature_record_file = open(self.temperature_record_path, 'a')
         self.temperature_record_file.write('box_temp,cpu_temp,time\n')
+        self.temperature_record_file.flush()
 
     def setup_gps_record(self):
         self.gps_record_file = open(self.gps_record_path, 'a')
         self.gps_record_file.write('lat,lon,alt,speed,climb,track,time,error\n')
+        self.gps_record_file.flush()
 
+    def setup_pps_record(self):
+        self.pps_record_file = open(self.pps_record_path, 'a')
+        self.pps_record_file.write('id, time\n')
+        self.pps_record_file.flush()
 
     def record_sensor_data(self, data):
         transformed_data = [str(x) for x in data]
@@ -45,6 +55,15 @@ class DataRecorder:
         transformed_data = [str(x) for x in data]
         self.gps_record_file.write(','.join(transformed_data) + '\n')
         self.gps_record_file.flush()
+
+    def record_pps_data(self, data):
+        transformed_data = [str(x) for x in data]
+        self.pps_record_file.write(','.join(transformed_data) + '\n')
+        self.pps_record_file.flush()
+
+    def record_multiple_pps_data(self, data):
+        for i in range(len(data)):
+            self.record_pps_data(data[i])
 
     def record_temperature_data(self, data):
         transformed_data = [str(round(x, 2)) for x in data]
